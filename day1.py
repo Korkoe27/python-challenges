6f204aa6166e3e40574dbf840f9f055c6ed70e8c
Mechanic <dumashieanthony@gmail.com>
2024-03-05 17:22:52 +0000

    added challenge instructions

--- day1.py
+++ day1.py
@@ -1,3 +1,11 @@
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+
+
 def sortLists(list1, list2):
     list1.sort()
     list2.sort()
@@ -6,7 +14,7 @@ def sortLists(list1, list2):
     print(list3)
 
 
-sortLists([23,923,3948,238], [238,434,32,53,1,354])
+sortLists([1,2,4], [1,3,4])
 
 
 # list1 = [19,309,42580,234,321,2343]
