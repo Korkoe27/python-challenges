9fa010f670e4e52bda02bea326dbbe73bc6d51af
Mechanic <dumashieanthony@gmail.com>
2024-03-05 17:09:16 +0000

    Day 1 of doing leetcode challenges. Simple sorting and merging program

---
+++ day1.py
@@ -0,0 +1,16 @@
+def sortLists(list1, list2):
+    list1.sort()
+    list2.sort()
+    list3 = list1+list2
+    list3.sort()
+    print(list3)
+
+
+sortLists([23,923,3948,238], [238,434,32,53,1,354])
+
+
+# list1 = [19,309,42580,234,321,2343]
+
+# list1.sort
+
+# print(list1)
